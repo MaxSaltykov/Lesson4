f616c9629d66f6b8216e0f037d11af01d094847a
Skuld <skuld@list.ru>
2022-03-18 00:35:04 +0300

    добавление allure

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -6,8 +6,12 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepDefinitions"},
+        plugin = {"pretty", "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "json:target/cucumber,json"},
         tags = "@TEST"
 )
 public class RunnerTest {
 }
+/**
+ * Если запустить тест отсюда, то аллюр отчёт построится
+ */
 
